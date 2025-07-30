5f90a85cd43c649433a951ae4c837fb869495133
ywkao <ywkao@hep1.phys.ntu.edu.tw>
2025-07-30 09:23:05 +0200

    add log info to inspect configurable MESSAGE_SIZE_LIMIT

--- src/cpp/DQM/DQMNet.cc
+++ src/cpp/DQM/DQMNet.cc
@@ -974,6 +974,11 @@ DQMNet::DQMNet(const std::string &appname /* = "" */)
   upstream_.next = downstream_.next = 0;
   upstream_.port = downstream_.port = 0;
   upstream_.update = downstream_.update = false;
+
+  logme() << "[INFO] dqmgui_prod/src/cpp/DQM/DQMNet.cc::MESSAGE_SIZE_LIMIT: "
+          << (MESSAGE_SIZE_LIMIT / (1024 * 1024)) << " MB, "
+          << "SOCKET_BUF_SIZE: " << (SOCKET_BUF_SIZE / (1024 * 1024)) << " MB"
+          << std::endl;
 }
 
 DQMNet::~DQMNet(void) {
